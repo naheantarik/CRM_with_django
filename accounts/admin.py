d6ebd58faed62997a2a6d85672b88490ba34c72e
naheantarik <nahean.swe@gmail.com>
2023-02-27 00:45:32 +0600

    Model Change

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
 
-from .models import Customer
-
 admin.site.register(Customer)
+admin.site.register(Product)
+admin.site.register(Order)
--- accounts/models.py
+++ accounts/models.py
@@ -11,3 +11,27 @@ class Customer(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Product(models.Model):
+    CATEGORY = (
+        ('Indoor Item', 'Indoor Item'),
+        ('Outdoor Item', 'Outdoor Item'),
+    )
+    name = models.CharField(max_length=200, null=True)
+    price = models.FloatField(null=True)
+    category = models.CharField(max_length=200, null=True, choices=CATEGORY)
+    Description = models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+
+class Order(models.Model):
+    STATUS = (
+        ('Peinding', 'Pending'),
+        ('On Process', 'On Process'),
+        ('On Delivery', 'On Delivery')
+        ('Delivered', 'Delivered'),
+    )
+
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    status = models.CharField(max_length=200, null=True, choices=STATUS)

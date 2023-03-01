463057ad2b22a16d8171ea3172b55a60d036a7e9
naheantarik <nahean.swe@gmail.com>
2023-03-02 03:46:34 +0600

    Initial

--- accounts/form.py
+++ accounts/form.py
@@ -1,8 +1,17 @@
 from django.forms import ModelForm
 from .models import Order
+from .models import Product
+
+# Create your Form here.
 
 
 class OrderForm(ModelForm):
     class Meta:
         model = Order
+        fields = '__all__'
+
+
+class ProductForm(ModelForm):
+    class Meta:
+        model = Product
         fields = '__all__'

91468256dc0c4ba872e7fff464dfb34f1ecd03be
naheantarik <nahean.swe@gmail.com>
2023-03-02 03:12:32 +0600

    Initial

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('product/', views.product, name='product'),
     path('customer/<str:cng>/', views.customer, name='customer'),
+
     path('order/', views.createOrder, name='create_order'),
+    path('update_order/<str:pk>/', views.update_order, name='update_order'),
 ]

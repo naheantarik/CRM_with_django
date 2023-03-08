6c42459cb883b79b4fe28d3af818f0bf635833ac
naheantarik <nahean.swe@gmail.com>
2023-03-09 02:21:09 +0600

    initial

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+
+
 from .models import *
 from .form import OrderForm, ProductForm, CreateCustomer, CreateUserForm
 from .filters import OrderFilters, ProductFielters
@@ -9,12 +13,28 @@ from .filters import OrderFilters, ProductFielters
 
 def registerPage(request):
     form = CreateUserForm()
+
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Account was created for' + user)
+
+            return redirect('/login')
+
     context = {'form': form}
     return render(request, 'accounts/register.html', context)
 
 
 def login(request):
-    context = {'form': form}
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        authenticate(request, username=username, password=password)
+
+    context = {}
     return render(request, 'accounts/login.html', context)
 
 

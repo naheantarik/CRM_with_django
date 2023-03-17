e0b0722ca28633df3d7c2736fdbb82a88ecb4593
naheantarik <nahean.swe@gmail.com>
2023-03-18 02:38:32 +0600

    initial

--- crm1/settings.py
+++ crm1/settings.py
@@ -123,7 +123,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static/css')
+    os.path.join(BASE_DIR, 'static')
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
